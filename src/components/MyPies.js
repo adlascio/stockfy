3de1d4d35b1eb5ebb4ee85cdc1cbef8d1ca5a1d5
Arthur Di Lascio <arthur.dilascio@gmail.com>
2022-03-28 11:47:46 -0700

    transactions sync with db completed + added pies form

--- src/components/MyPies.js
+++ src/components/MyPies.js
@@ -1,7 +1,161 @@
-import React from 'react'
+import React , { useRef, useState } from 'react'
+import { Container, Form, Button, Card, Table } from 'react-bootstrap';
+import { db } from '../firebase';
+import {addDoc, collection} from 'firebase/firestore'
 
 export default function MyPies() {
+  const nameRef = useRef();
+  const stockNameRef = useRef();
+  const [selectedPie, setSelectedPie] = useState();
+  const [alocated, setAlocated] = useState(0);
+  const [slicesArr, setSlicesArr] = useState([]);
+  const [selectedOption, setSelectedOption] = useState("stocks/etfs");
+  const piesCollectionRef = collection(db, "Pies");
+
+  const increaseTarget = (e, index) => {
+    let slicesArrCopy = [...slicesArr];
+    slicesArrCopy[index].target += 1;
+    setSlicesArr(slicesArrCopy);
+    setAlocated(alocated+1);  
+  }
+
+  const decreaseTarget = (e, index) => {
+    if(slicesArr[index].target === 0) return;
+    let slicesArrCopy = [...slicesArr];
+    slicesArrCopy[index].target -= 1;
+    setSlicesArr(slicesArrCopy);
+    setAlocated(alocated-1);  
+  }
+  
+  const handleSelectedOption = (e) => {
+    setSelectedOption(e.target.value);
+  }
+
+  const handleSelectPie = (e) => {
+    setSelectedPie(e.target.value);
+  }
+
+  const addPie = (e) => {
+    e.preventDefault();
+    setSelectedPie(e.target.value);
+    const newSlice = {
+      name: e.target.value,
+      target: 0,
+      pieId: "test"
+    }
+    setSlicesArr([...slicesArr, newSlice]);
+  }
+
+  const addSlice = (e) => {
+    e.preventDefault();
+    const newSlice = {
+      name: stockNameRef.current.value,
+      target: 0,
+      pieId:null
+    }
+    stockNameRef.current.value = "";
+    setSlicesArr([...slicesArr, newSlice]);
+  }
+
+  const deleteSlice = (e, index) => {
+    let slicesArrCopy = [...slicesArr];
+    setAlocated(alocated-slicesArrCopy[index].target);
+    slicesArrCopy.splice(index, 1);
+    setSlicesArr(slicesArrCopy);
+  }
+
+  const createPie = async (e) => {
+    e.preventDefault();
+    const pie = {
+      name:nameRef
+    }
+    try{  
+      await addDoc(piesCollectionRef, pie)
+    } catch(e){
+      console.log("error at adding new pie", e);
+    }
+    
+  }
   return (
-    <div>MyPies</div>
+    <Container>
+      <h3 className='text-center mb-4'>My Pies</h3>
+      <Card>
+        <Card.Body>
+        <h4 className='text-center mb-4'>Create a new Pie</h4>
+          <Form>
+            <Form.Group className='signup__form-group' id='email'>
+                <Form.Label>Name:</Form.Label>
+                <Form.Control type='text' ref={nameRef} required />
+            </Form.Group>
+          </Form>
+          <h4 className='text-center mb-4 mt-4'>My Slices</h4>
+          <div className='slices__options'>
+            <button className='slices__option' value="stocks/etfs" onClick={e => handleSelectedOption(e)}>Stocks / ETF</button>
+            <button className='slices__option' value="myPies" onClick={e => handleSelectedOption(e)}>My Pies</button>
+          </div>
+          <Form>
+            {selectedOption === "myPies"? 
+              (
+                <>
+                  <Form.Group className='signup__form-group' id='email'>
+                    <Form.Label>Add an existing pie: </Form.Label>
+                    <Form.Select onChange={(e) => handleSelectPie(e)}>
+                      <option>Open this select menu</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </Form.Select>
+                  </Form.Group>
+                  <Button className='mt-4' variant='success' type='submit' onClick={e => {if(e.target.value !== "") addPie(e)}}>Add Pie</Button>
+                </>
+                
+              ): 
+              (
+                <>
+                  <Form.Group className='signup__form-group' id='email'>
+                    <Form.Label>Choose a stock or ETF to add:</Form.Label>
+                    <Form.Control type='text' ref={stockNameRef} required />
+                  </Form.Group>
+                  <Button className='mt-4' variant='success' type='submit' onClick={e => addSlice(e)}>Add Stock / ETF</Button>
+                </>
+              )} 
+          </Form>
+          <p style={{float:"right"}}>Total Alocated: {alocated}/100</p>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Slice</th>
+                <th>Target</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {slicesArr.map((slice, index) => (
+                <tr key={index} className="slices__row">
+                  <td>{index+1}</td>
+                  <td>{slice.name}</td>
+                  <td>
+                    <div className='target'>
+                      <div>
+                        {slice.target} %
+                      </div>
+                      <div className='targetHandler'>
+                        <i className="uil uil-plus-circle" onClick={e =>  increaseTarget(e,index)}></i>
+                        <i className="uil uil-minus-circle" onClick={e => decreaseTarget(e,index)}></i>
+                      </div> 
+                    </div>
+                    </td>
+                    <td>
+                      <i className="uil uil-trash-alt" onClick={e => deleteSlice(e, index)}></i>
+                    </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+          <Button className='mt-4' variant='success' type='submit' onClick={e => createPie(e)}>Create new Pie</Button>
+        </Card.Body>
+      </Card>  
+    </Container>
   )
 }
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,38 +1,94 @@
-import React, { useRef, useState } from 'react'
-import {addDoc, collection} from 'firebase/firestore'
+import React, { useEffect, useRef, useState } from 'react'
+import {addDoc, collection, deleteDoc, getDocs, doc} from 'firebase/firestore'
 import { db } from '../firebase';
-import { Container, Form, Button, Card } from 'react-bootstrap';
+import { Container, Form, Button, Card, Table, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext';
 
 export default function Transactions() {
   const transactionsCollectionRef = collection(db, "Transactions");
+  const [transactions, setTransactions] = useState({
+    list:[],
+    isLoading:false
+  });
   const stockCodeRef = useRef();
   const quantityRef = useRef();
   const priceRef = useRef();
   const transactionDateRef = useRef();
   const [action, setAction] = useState("Buy");
+  const [error, setError] = useState("");
+  const user = useAuth();
   const createTransaction = async (e) => {
     e.preventDefault();
+    setTransactions({...transactions, isLoading:true})
+    const today = new Date();
+    if(stockCodeRef.current.value === "") {
+      setError("Error: Invalid Stock / ETF code")
+      setTransactions({...transactions, isLoading:false})
+      return
+    }
+    if(priceRef.current.value === "" || priceRef.current.value<=0) {
+      setError("Error: Invalid price value")
+      setTransactions({...transactions, isLoading:false})
+      return
+    }
+    if(quantityRef.current.value === "" || quantityRef.current.value<=0) {
+      setError("Error: Invalid quantity")
+      setTransactions({...transactions, isLoading:false})
+      return
+    }
+    if(transactionDateRef.current.value === "") {
+      setError("Error: Invalid transaction date")
+      setTransactions({...transactions, isLoading:false})
+      return
+    }
     const transaction = {
       action,
       stock_code: stockCodeRef.current.value,
       price: priceRef.current.value,
       quantity: quantityRef.current.value,
       transaction_date: transactionDateRef.current.value,
+      userId: user.currentUser.uid,
     }
-    try{  
-      await addDoc(transactionsCollectionRef, transaction)
+    console.log("transaction",transaction)
+    try{
+      await addDoc(transactionsCollectionRef, transaction);
+      await getTransactions();
     } catch(e){
       console.log("error at adding new transaction", e);
     }
     
   }
 
+  const getTransactions = async () => {
+    const data = await getDocs(transactionsCollectionRef);
+    const updatedTransactionsList = data.docs.map(doc => ({...doc.data(), id:doc.id}))
+    setTransactions({
+      ...transactions,
+      list:updatedTransactionsList
+    });
+  };
+
+  const deleteTransaction = async(index, id) => {
+    try{
+      const transactionDoc = doc(db, "Transactions", id);
+      await deleteDoc(transactionDoc);
+      await getTransactions();
+    }catch(e){
+      console.log("error at deleting transaction", e);
+    }
+  }
+
+  useEffect(() => {
+    getTransactions();
+  }, [])
+
   return (
     <Container>
-      <h4>Add Transactions</h4>
-      <Card>
+      <h4 className='text-center mb-4'>Add Transactions</h4>
+      <Card className='mb-4'>
         <Card.Body>
           <Form>
+            {error && <Alert variant='danger'>{error}</Alert>}
             <Form.Group className='signup__form-group' id='email'>
                 <Form.Label>Stock code</Form.Label>
                 <Form.Control type='text' ref={stockCodeRef} required />
@@ -57,9 +113,35 @@ export default function Transactions() {
                 <Form.Control type='date' ref={transactionDateRef} required />
             </Form.Group>
           </Form>
-          <Button className='mt-4' variant='success' type='submit' onClick={e => createTransaction(e)}>Submit</Button>
+          <Button className='mt-4' variant='success' type='submit' onClick={e => createTransaction(e)} disabled={transactions.isLoading}>Submit</Button>
         </Card.Body>
-      </Card>     
+      </Card>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Stocks / ETFs</th>
+            <th>Quantity</th>
+            <th>Price</th>
+            <th>Transaction Date</th>
+            <th>Type</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.list.map((transaction, index) => (
+            <tr key={index} className="slices__row">
+              <td>{index+1}</td>
+              <td>{transaction.stock_code}</td>    
+              <td>{transaction.quantity}</td>    
+              <td>{transaction.price}</td>    
+              <td>{transaction.transaction_date}</td>    
+              <td>{transaction.action}</td>
+              <td> <i className="uil uil-trash-alt" onClick={() => deleteTransaction(index, transaction.id)}></i></td>
+            </tr>
+              ))}
+            </tbody>
+        </Table>    
     </Container>
   )
 }

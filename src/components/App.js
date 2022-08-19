81a077a054ff89372a4420a5912a30e59470079d
adlascio <arthur.dilascio@gmail.com>
2022-08-19 00:30:56 -0700

    fixed issues in transactions

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,8 @@ import '../App.css';
 import Dashboard from './Dashboard';
 import Signup from './Signup';
 import Login from './Login';
-import { AuthProvider} from '../contexts/AuthContext';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PrivateRoute from './PrivateRoute';
 import ForgotPassword from './ForgotPassword';
 import UpdateProfile from './UpdateProfile';
@@ -17,70 +17,69 @@ import Profile from './Profile';
 
 function App() {
   return (
-      <div className='app'>
-        <Router>
-          <AuthProvider>
-            <Aside />
-            <div>
-              <Routes>
-                <Route 
-                  exact path='/' 
-                  element={
-                    <PrivateRoute>
-                      <Dashboard/>
-                    </PrivateRoute>                    
-                  }
-                />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route 
-                  path='/update-profile' 
-                  element={
-                    <PrivateRoute>
-                      <UpdateProfile/>
-                    </PrivateRoute>                    
-                  }
-                />
-                <Route 
-                  path='/transactions' 
-                  element={
-                    <PrivateRoute>
-                      <Transactions/>
-                    </PrivateRoute>                    
-                  }
-                />
-                <Route 
-                  path='/my-pies' 
-                  element={
-                    <PrivateRoute>
-                      <MyPies/>
-                    </PrivateRoute>                    
-                  }
-                />
-                <Route 
-                  path='/portfolio' 
-                  element={
-                    <PrivateRoute>
-                      <Portfolio/>
-                    </PrivateRoute>                    
-                  }
-                />
-                <Route 
-                  path='/profile' 
-                  element={
-                    <PrivateRoute>
-                      <Profile/>
-                    </PrivateRoute>                    
-                  }
-                />
-              </Routes>
-            </div>
-            
-          </AuthProvider>
-        </Router>
-      </div>
-        
+    <div className='app'>
+      <Router>
+        <AuthProvider>
+          <Aside />
+          <div>
+            <Routes>
+              <Route
+                exact
+                path='/'
+                element={
+                  <PrivateRoute>
+                    <Dashboard />
+                  </PrivateRoute>
+                }
+              />
+              <Route path='/signup' element={<Signup />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/forgot-password' element={<ForgotPassword />} />
+              <Route
+                path='/update-profile'
+                element={
+                  <PrivateRoute>
+                    <UpdateProfile />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path='/transactions'
+                element={
+                  <PrivateRoute>
+                    <Transactions />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path='/my-pies'
+                element={
+                  <PrivateRoute>
+                    <MyPies />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path='/portfolio'
+                element={
+                  <PrivateRoute>
+                    <Portfolio />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path='/profile'
+                element={
+                  <PrivateRoute>
+                    <Profile />
+                  </PrivateRoute>
+                }
+              />
+            </Routes>
+          </div>
+        </AuthProvider>
+      </Router>
+    </div>
   );
 }
 
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,52 +1,66 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react';
 import { auth } from '../firebase';
-import { createUserWithEmailAndPassword , signOut, onAuthStateChanged, signInWithEmailAndPassword, sendPasswordResetEmail, updatePassword} from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  sendPasswordResetEmail,
+  updatePassword,
+} from 'firebase/auth';
 
 const AuthContext = React.createContext();
 
-export function useAuth(){
-    return useContext(AuthContext);
+export function useAuth() {
+  return useContext(AuthContext);
 }
 
-export function AuthProvider({children}) {
-    const [currentUser, setCurrentUser] = useState();
-    const [loading, setLoading] = useState(true);
+export function AuthProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+  console.log('auth', auth);
+  function signup(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password);
+  }
 
-    function signup(email, password){
-      return createUserWithEmailAndPassword(auth,email, password)
-    }
+  function login(email, password) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
 
-    function login(email, password){
-      return signInWithEmailAndPassword(auth,email, password)
-    }
+  const logout = () => {
+    return signOut(auth);
+  };
 
-    const logout = () => {
-      return signOut(auth);
-    }
+  function resetPassword(email) {
+    return sendPasswordResetEmail(auth, email);
+  }
 
-    function resetPassword(email) {
-      return sendPasswordResetEmail(auth, email);
-    }
+  function updateUserPassword(password) {
+    return updatePassword(currentUser, password);
+  }
 
-    function updateUserPassword(password) {
-      return updatePassword(currentUser, password);
-    }
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      // dispatch(setActiveUser(user))
+      console.log(user);
+      setCurrentUser(user);
+      setLoading(false);
+    });
+    return unsubscribe;
+  }, []);
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, user => {
-            // dispatch(setActiveUser(user))
-            console.log(user)
-            setCurrentUser(user);
-            setLoading(false)
-        })
-        return unsubscribe
-    }, [])
-
-    const value = {currentUser, signup, logout, login, resetPassword, updateUserPassword}
+  const value = {
+    currentUser,
+    signup,
+    logout,
+    login,
+    resetPassword,
+    updateUserPassword,
+  };
 
   return (
     <AuthContext.Provider value={value}>
-        {!loading && children}
+      {!loading && children}
     </AuthContext.Provider>
-  )
+  );
 }
